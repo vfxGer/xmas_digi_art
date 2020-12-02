efe745ad11c0c061de50bdfe4f6382139cfe3abc
Gerard Keating <gerard.keating@rapidratings.com>
2020-12-02 13:06:14 +0000

    Still wierd results from math plot imsave

--- create_images.py
+++ create_images.py
@@ -3,9 +3,9 @@ import random
 from pprint import pprint
 from sys import maxsize
 
-WIDTH = 10
-HEIGHT = 10
-NUM_SPOTS = 5
+WIDTH = 1024
+HEIGHT = 1024
+NUM_SPOTS = 100
 
 def create_random_spots():
 	x = random.sample(range(WIDTH), NUM_SPOTS)
@@ -14,43 +14,59 @@ def create_random_spots():
 	points.sort()
 	colours = {}
 	for p in points:
-		colours[p] = random.sample(range(0, 255), 3)
+		colours[p] = random.sample(range(255), 3)
 	return colours
 
 
 def squared_dist(p1, p2):
 	res = (p1[0] - p2[0])**2 + (p1[1] - p2[1])**2
+	# print("(%d - %d)**2 + (%d - %d)**2=%d"%(p1[0], p2[0], p1[1], p2[1], res) )
 	# print(p1)
 	# print(p2)
 	# print(res)
-	# print("*****")
 	return res
+
+
+def test():
+	row = [0] * 10
+	img_arr = []
+	for _ in range(10):
+		img_arr.append(row.copy())
+	pprint(img_arr)
+	img_arr[0][0] = 72
+	pprint(img_arr)
+	assert img_arr[1][0]==0
 
 
 def main():
 	colours = create_random_spots()
-	img = [[0] * (WIDTH)] * (HEIGHT)
-	# pprint(colours)
-	for x in range(WIDTH):
-		for y in range(HEIGHT):
+	row = [0] * WIDTH
+	img_arr = []
+	for _ in range(HEIGHT):
+		img_arr.append(row.copy())
+	pprint(colours)
+	# pprint(img_arr)
+	for y in range(WIDTH):
+		for x in range(HEIGHT):
+			# print([x,y])
 			min_dist = maxsize
 			for p in colours:
 				d = squared_dist([x,y], p)
 				if d < min_dist:
 					min_dist = d
-					if d==0:
-						print((x,y))
-						img[x][y] = [255, 0, 0]
-					else:
-						img[x][y] = colours[p]
-					print("----------")
-		
-		# print(((x+1)/WIDTH)*100)
-	pprint(img)
+					# if d==0:
+					# # # 	print((x,y))
+					#   	img_arr[y][x] = "x"
+					# else:
+					img_arr[y][x] = colours[p]
+					# print("Setting %d, %d to %d"%(x,y,colours[p]))
+					# print("----------")
+		print( "%.2f%%" %(((y+1)/WIDTH)*100))
+	# pprint(img_arr)
 	# red = (1.0,0.0,0.0)
 	# redline = [red] * 1024
 	# redimage = [redline] * 1024
-	imsave("ver1.jpg", img)
+	imsave("ver1.png", img_arr)
 
 if __name__=="__main__":
 	main()
